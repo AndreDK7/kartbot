afdc48d152f252caa6cc04e1dd82a24dc9016287
AndreDK7 <gomesnegreiros@gmail.com>
2026-08-13 14:59:47 -0300

    Adicionado o addon printresults.lua para mostrar o resultado das corridas na ponte do Discord.

---
+++ printresults.lua
@@ -0,0 +1,26 @@
+local cv_printresults = CV_RegisterVar({
+    name = "printresults",
+    defaultvalue = "On",
+    possiblevalue = CV_OnOff,
+    flags = CV_NETVAR,
+})
+
+local printresults = false
+
+addHook("IntermissionThinker", do
+    if printresults then
+        local str = ";"
+        for i = 1, #players - 1 do
+            local player = players[i]
+            if player != nil then
+                str = str + tostring(i) + ":" + player.name + ":" + tostring(player.kartstuff[k_position]) + ":" + tostring(player.realtime) + ":" + tostring(player.lives == 0) + ":" + tostring(player.mo == nil) + ";"
+            end
+        end
+        CONS_Printf(server, "[RESULTS] " + str)
+        printresults = false
+    end
+end)
+
+addHook("MapLoad", do
+    printresults = cv_printresults.value
+end)
